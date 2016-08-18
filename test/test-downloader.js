fc3594dc61437ba29e7cc2fa3030334840f09f6a
Juho Kolehmainen <juho.kolehmainen@iki.fi>
2016-08-18 08:23:37 +0300

    Refactoring pdf parsing and added tests.

--- test/test-downloader.js
+++ test/test-downloader.js
@@ -32,7 +32,7 @@ describe('Downloader', () => {
     ws.reply(200, (uri, requestBody, cb) => {
       fs.readFile('./test/resources/AMDT_XX_2016_iso_eaip_finland_14aug2016.iso', cb)
     })
-    return downloader(url, 'data', 'TEST').should.be.fulfilled.then(() => {
+    return downloader.start(url, 'data', 'TEST').should.be.fulfilled.then(() => {
       fs.existsSync('./data/TEST').should.equal(true)
       fs.readFileSync('./data/TEST/aerodromes/EF_AD_2_EFHF_EN.pdf', 'utf8').should.equal('EFHF')
       fs.readFileSync('./data/TEST/enr/EF_ENR_1_1_EN.pdf', 'utf8').should.equal('ENR')
---
+++ test/test-eaip.js
@@ -0,0 +1,54 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const eaip = require('../lib/eaip')
+const downloader = require('../lib/downloader')
+const nock = require('nock')
+const fs = require('fs')
+const path = require('path')
+
+chai.should()
+
+const TMP_DIR = path.join(__dirname, '../tmp')
+
+const aerodromes = `${TMP_DIR}/EF_AD_2_EFHK_EN.pdf`
+const airspaces = `${TMP_DIR}/EF_ENR_2_1_EN.pdf`
+const dangers = `${TMP_DIR}/EF_ENR_5_1_EN.pdf`
+
+function* downloadTestFiles() {
+  nock('https://ais.fi', { allowUnmocked: true })
+    .get('ais/eaip/pdf/**.pdf')
+    .reply(200, 'OK!');
+
+  if (!fs.existsSync(aerodromes)) {
+    yield downloader.downloadFile('https://ais.fi/ais/eaip/pdf/aerodromes/EF_AD_2_EFHK_EN.pdf', aerodromes)
+  }
+
+  if (!fs.existsSync(airspaces)) {
+    yield downloader.downloadFile('https://ais.fi/ais/eaip/pdf/enr/EF_ENR_2_1_EN.pdf', airspaces)
+  }
+
+  if (!fs.existsSync(dangers)) {
+    yield downloader.downloadFile('https://ais.fi/ais/eaip/pdf/enr/EF_ENR_5_1_EN.pdf', dangers)
+  }
+}
+
+let init = downloadTestFiles()
+while(!init.next().done) {
+}
+
+describe('Parse AIP files', () => {
+  it('should parse EFHK CTR', () => {
+    const expected = JSON.parse(fs.readFileSync(`${__dirname}/resources/EF_AD_2_EFHK_EN.json`, 'utf8'))
+
+    const files = (glob) => {
+      if (glob.indexOf("aerodromes") == 0) {
+        return Promise.resolve([aerodromes])
+      } else {
+        return Promise.resolve([])
+      }
+    }
+    return eaip.refresh({cycle: 'test', validFrom: '', validUntil: '', files}).then((res) => {
+      res.should.deep.equal(expected)
+    })
+  })
+})
